9e08eebd5b150cfbbac6e379815e901cb0b42eb0
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-08-12 19:23:06 +0000

    create welcome method for cli

--- lib/austin/cli.rb
+++ lib/austin/cli.rb
@@ -7,6 +7,15 @@ class CLI
     start_selection
   end 
   
+  def welcome 
+    puts "" 
+    puts "                         Welcome to Austin, Texas!"
+    puts "" 
+    puts "                   The Live Music Capital of the World!"
+    puts "" 
+    puts "" 
+    puts "Enter the number of the attraction you are interested in, for more information." 
+  end 
   
   
 end 
