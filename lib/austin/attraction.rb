542edb6e0b3f360eec59392126064d84e7c47b51
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-08-23 03:38:01 +0000

    update all lib/austin files

--- lib/austin/attraction.rb
+++ lib/austin/attraction.rb
@@ -1,13 +1,11 @@
 class Austin::Attraction
   
-  attr_accessor :name, :type, :price, :url, :index, :description, :open_hours 
+  attr_accessor :name, :url, :index, :description, :open_hours 
   
   @@all = [] 
   
   def initialize(name, url, index)
     @name = name 
-    # @type = type 
-    # @price = price 
     @url = url 
     @index = index 
     @@all << self 
--- lib/austin/scraper.rb
+++ lib/austin/scraper.rb
@@ -7,8 +7,6 @@ class Austin::Scraper
     doc.css("#FILTERED_LIST li").each.with_index(1) do |att, index| 
       name = att.css("a")[2].text 
       url = att.css("a")[0].attr("href") 
-      # type = att.css("span")[0].text 
-      # price = att.css("span")[0].text 
       attraction = Austin::Attraction.new(name, url, index)
     end 
   end 
@@ -18,6 +16,7 @@ class Austin::Scraper
     
     description = doc.css("#taplc_location_detail_about_card_0")[0].css("span")[0].text
     open_hours = doc.css("#taplc_location_detail_about_card_0")[0].css("div")[6].text
+    binding.pry
   end 
   
 end 
